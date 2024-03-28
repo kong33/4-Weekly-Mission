733acecb1efc9667b08afd48af6b74183c784380
kong33 <kge021114@naver.com>
2024-03-28 22:39:23 +0900

    ✨ Feat : Button, AddLink 컴포넌트 추가

---
+++ features/Button/index.tsx
@@ -0,0 +1,23 @@
+import '@/styles/reset.module.scss';
+import '@/styles/common.module.scss';
+import '@/features/Button/style.module.scss';
+
+interface ButtonProps {
+  title: string;
+  size: string;
+  isLoading: boolean;
+}
+
+const Button = ({ title, size, isLoading }: ButtonProps) => {
+  return (
+    <button
+      type="submit"
+      className={`CTA ${size}`}
+      style={isLoading ? { display: 'none' } : { display: 'block' }}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;

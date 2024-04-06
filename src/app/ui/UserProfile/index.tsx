b7e37b462d45e52d31e05933db1b46d23cfc4fd8
gaeun <kge0211114@gmail.com>
2024-04-06 18:57:13 +0900

    Feat : public/images, svgs 추가 , userProfile 추가(저장 안된 파일 다시 commit)

--- src/app/ui/UserProfile/index.tsx
+++ src/app/ui/UserProfile/index.tsx
@@ -1,19 +1,19 @@
-import useGetProfile from '../../model/useGetProfile';
-import Image from 'next/image';
-import styles from './style.module.scss';
+// import useGetProfile from './useGetProfile';
+// import Image from 'next/image';
+// import styles from './style.module.scss';
 
-const UserProfile = () => {
-  const { ownerImage, ownerName, folderName } = useGetProfile();
+// const UserProfile = () => {
+//   const { ownerImage, ownerName, folderName } = useGetProfile();
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.imgWrapper}>
-        {ownerImage && <Image src={ownerImage} alt="아바타" fill />}
-      </div>
-      <p className={styles.ownerName}>{ownerName}</p>
-      <h1 className={styles.folderName}>{folderName}</h1>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.imgWrapper}>
+//         {ownerImage && <Image src={ownerImage} alt="아바타" fill />}
+//       </div>
+//       <p className={styles.ownerName}>{ownerName}</p>
+//       <h1 className={styles.folderName}>{folderName}</h1>
+//     </div>
+//   );
+// };
 
-export default UserProfile;
+// export default UserProfile;

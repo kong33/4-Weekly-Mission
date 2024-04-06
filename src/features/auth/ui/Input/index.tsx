c2471375864da68c1fa87e7fe3f3fe23d6b69b60
깅강응 <ging-gang-eung@ging-gang-eung-ui-MacBookPro.local>
2024-04-06 16:32:07 +0900

    세팅 초기화

--- src/features/auth/ui/Input/index.tsx
+++
@@ -1,7 +0,0 @@
-interface inputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = (props: inputProps) => {
-  return <input {...props}></input>;
-};
-
-export default Input;

41d2f2afd48fbf2b5c2541fef8fdd7eb7f82d479
gaeun <kge0211114@gmail.com>
2024-04-07 13:37:18 +0900

    Feat : CardList 컴포넌트 추가

---
+++ src/pages/folder/CardList/index.tsx
@@ -0,0 +1,37 @@
+import styles from './style.module.scss';
+import Card from '@/app/ui/Card';
+
+const CardList = ({ items }: { items: Item[] | null | undefined }) => {
+  if (!(items && items.length))
+    return (
+      <div className={styles.null_container}>
+        <p> 저장된 링크가 없습니다. </p>
+      </div>
+    );
+  return (
+    <div className={styles.container}>
+      {items.map((item: Item) => (
+        <Card
+          createdAt={item.createdAt}
+          imageSource={item.imageSource}
+          description={item.description}
+          key={item.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
+
+export interface Item {
+    id: number;
+    createdAt: string;
+    updated_at?: string;
+    url: string;
+    title?: string;
+    description?: string;
+    imageSource?: string;
+    folder_id?: number;
+  }
+  
